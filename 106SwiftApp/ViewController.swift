a6944972e02ff5d7c5f55e987a1f060bde91ed8b
JeranKilpack <31513821+JeranKilpack@users.noreply.github.com>
2017-10-06 09:21:13 -0600

    Added random screen changing color.

--- 106SwiftApp/ViewController.swift
+++ 106SwiftApp/ViewController.swift
@@ -18,8 +18,32 @@ class ViewController: UIViewController
 
     @IBAction func firstMethod(_ sender: UIButton)
     {
-        
+        if (firstButton.backgroundColor == .red)
+        {
+            firstButton.backgroundColor = .green
+        }
+        else
+        {
+            firstButton.backgroundColor = .red
+        }
+        view.backgroundColor = createRandomColor()
     }
+    
+    
+    
+    
+    private func createRandomColor() -> UIColor
+    {
+        let newColor :UIColor
+        let redAmount = CGFloat (Double (arc4random_uniform(256))/255.00)
+        let greenAmount = CGFloat (Double (arc4random_uniform(256))/255.00)
+        let blueAmount = CGFloat (Double (arc4random_uniform(256))/255.00)
+        newColor = UIColor(red: redAmount, green: greenAmount, blue: blueAmount, alpha: CGFloat(1.0))
+        
+        return newColor
+    }
+    
+    
     override func didReceiveMemoryWarning()
     {
         super.didReceiveMemoryWarning()

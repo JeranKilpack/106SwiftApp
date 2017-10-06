0b18e3264e459cad772e9da05c75e555c823bb83
JeranKilpack <31513821+JeranKilpack@users.noreply.github.com>
2017-10-06 08:53:14 -0600

    Added IBAction and IBOutlet to my view controller.

--- 106SwiftApp/ViewController.swift
+++ 106SwiftApp/ViewController.swift
@@ -8,14 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController
+{
+    @IBOutlet weak var firstButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+    @IBAction func firstMethod(_ sender: UIButton)
+    {
+        
+    }
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
